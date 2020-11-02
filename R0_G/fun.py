59926c5ab7e3dd79c2f909a53600851feeb4339b
Susan Anyosa <susan.anyosa@ntnu.no>
2020-11-02 15:16:53 +0100

    uploading files

---
+++ R0_G/fun.py
@@ -0,0 +1,129 @@
+import numpy as np
+import pandas as pd
+import os
+import re
+import random
+import math
+
+
+def distance_grid_loc(n1: int, n2: int, sq: bool):
+    n=n1*n2
+    vv = np.array(range(n1))
+    vv_ones = np.ones(n1)
+
+    uu = np.array(range(n2))
+    uu_ones = np.ones(n2)
+
+    s1 = np.dot(vv.reshape(n1, 1), uu_ones.reshape(1, n2))  # H_North
+    s2 = np.dot(vv_ones.reshape(n1, 1), uu.reshape(1, n2))  # s1.T H_East
+    a1 = s1.reshape(n)
+    a2 = s2.reshape(n)
+    # distance grid
+    ones = np.ones(n)
+    distance_matrix_a1 = np.dot(a1.reshape(n, 1), ones.reshape(1, n)) - np.dot(ones.reshape(n, 1), a1.reshape(1, n))
+    d2_a1 = distance_matrix_a1 * distance_matrix_a1
+    distance_matrix_a2 = np.dot(a2.reshape(n, 1), ones.reshape(1, n)) - np.dot(ones.reshape(n, 1), a2.reshape(1, n))
+    d2_a2 = distance_matrix_a2 * distance_matrix_a2
+    H = np.sqrt(d2_a1 + d2_a2)
+    if{sq==True}:
+        H=d2_a1 + d2_a2
+    return H
+
+
+def create_index(m: int, samples: int, k: int):
+    index_test = []
+    for i in range(samples):
+        random.seed(i)
+        index_test.append(random.sample(range(0,m),k))
+    return index_test
+
+
+def split_by_index_R0_G(l_R0: list, l_G: list, index_test: list):
+    m = len(l_R0)
+    index_test = index_test
+    index_train=[index for index in range(0,m) if index not in index_test]
+    l_R0_test = [ l_R0[index] for index in index_test ]
+    l_R0_train = [ l_R0[index] for index in index_train ]
+    l_G_test = [l_G[index] for index in index_test]
+    l_G_train = [l_G[index] for index in index_train]
+    return l_R0_train, l_R0_test, l_G_train, l_G_test
+
+
+def arguments_gp_R0_G(H, phi, l_train_R0, l_train_G):
+    R = np.kron(np.exp(-phi * H), np.array([[1,-0.6],[-0.6,1]]))
+    df_train_R0 = ltodf(l_train_R0)
+    df_train_G = ltodf(l_train_G)
+
+    m_train_R0, cov_R0 = gmcov(df_train_R0)
+    m_train_G, cov_G = gmcov(df_train_G)
+    cov = np.concatenate((cov_R0.diagonal(), cov_G.diagonal()), axis=None)
+    Sigma_train = np.dot(np.dot(np.sqrt(np.diag(cov)), R), np.sqrt(np.diag(cov)))
+    m_train = np.concatenate((m_train_R0, m_train_G), axis=None)
+
+    ld_train, il_train = ldil(Sigma_train)
+    return m_train, ld_train, il_train
+
+
+def ltodf(l: "list of arrays"):
+    l_to_df = pd.DataFrame()
+    for i in range(len(l)):
+        l_to_df = l_to_df.append(pd.DataFrame(l[i].reshape(1, l[i].size)), ignore_index=True)
+    return (l_to_df)
+
+
+def probabilities_gp(p: list):
+    lnp0 = p[0]
+    lnp1 = p[1]
+    num0 = len(str(int(math.modf(lnp0)[1])))-1
+    num1 = len(str(int(math.modf(lnp1)[1])))-1
+    num=min(num0,num1)
+    common_base = np.exp(10)**num
+    a0 = lnp0/(10**num)
+    a1 = lnp1/(10**num)
+    prob0 = common_base**a0/(common_base**a0 + common_base**a1)
+    prob1 = common_base**a1/(common_base**a0 + common_base**a1)
+    probabilities = [round(prob0, 5), round(prob1, 5)]
+    return probabilities
+
+
+class LogCP2:
+    def __init__(self, test, mu, ld, il, prior):
+        self.n = mu.shape[0]
+        self.test = test
+        self.mu = mu
+        self.prior = prior
+        self.ld = ld
+        self.il = il
+        self.u = np.dot(test - mu, il)
+
+    def value(self):
+        return np.log(self.prior) + (-0.5) * (np.dot(self.u, self.u.T) + self.n * np.log(2 * np.pi) + self.ld)
+
+
+def classif(l: list):
+    if l[0] > l[1]:
+        return 0
+    else:
+        return 1
+
+
+def gmcov(df):
+    mean_vector = np.array(df.mean(axis=0))
+    covariance_matrix = np.array(df.cov())
+    return (mean_vector, covariance_matrix)
+
+
+def ldil(Sigma):
+    L = np.linalg.cholesky(Sigma)
+    ld = 2 * sum(np.log(L.diagonal()))
+    il = np.linalg.inv(L)
+    return ld, il
+
+
+def voi_mc(conditional_probabilities: np.array, values):
+    expval_a_0 = values['v(x=0,a=0)'] * conditional_probabilities[...,0] + values['v(x=1,a=0)'] * conditional_probabilities[...,1]
+    expval_a_1 = values['v(x=0,a=1)'] * conditional_probabilities[...,0] + values['v(x=1,a=1)'] * conditional_probabilities[...,1]
+    pov = pd.DataFrame({'x=0': pd.Series(expval_a_0), 'x=1': pd.Series(expval_a_1)}).max(axis=1).mean()
+    pv = pd.DataFrame({'x=0': pd.Series(expval_a_0), 'x=1': pd.Series(expval_a_1)}).mean(axis=0).max()
+    voi = round(pov - pv, 5)
+    return voi
---
+++ R0_G/voi_R0_G.py
@@ -0,0 +1,148 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+import numpy as np
+import pandas as pd
+import fun
+from keras import layers
+from keras import models
+from keras import optimizers
+import pickle
+
+path = '.../R0_G/'
+
+#folder = '1802201656'
+folder = '30Jan20'
+#folder = '1502201536'
+
+f = open(path+'dict_'+folder+'_.pkl','rb')
+D = pickle.load(f)
+f.close()
+
+
+H = fun.distance_grid_loc(18, 18, True)
+phi = 3/10
+
+index_test_s1 = fun.create_index(m=496, samples=10, k=100)
+index_test_s2 = fun.create_index(m=504, samples=10, k=100)
+
+accuracy_array = np.zeros(shape=(45, 10))
+accuracy_array[0:5, 0:10] = np.repeat(0.5, 50).reshape(5,10)
+
+voi_array = np.zeros(shape=(45,10))
+voi_array[0:5, 0:10] = np.repeat(0, 50).reshape(5,10)
+
+gp_array = np.zeros(shape=(1600,1))
+probas_gp_array = np.zeros(shape=(1600,10))
+knn_array = np.zeros(shape=(1600,1))
+probas_knn_array = np.zeros(shape=(1600,10))
+rf_array = np.zeros(shape=(1600,1))
+probas_rf_array = np.zeros(shape=(1600,10))
+mlp_array = np.zeros(shape=(1600,1))
+probas_mlp_array = np.zeros(shape=(1600,10))
+cnn_array = np.zeros(shape=(1600,1))
+probas_cnn_array = np.zeros(shape=(1600,10))
+
+times = [2, 5, 8, 11, 14, 17, 20, 23]
+
+for j in range(10):
+    accuracy_dict = {}
+    voi_dict = {}
+
+    for t in range(len(D)):
+        l_scen1_train_R0, l_scen1_test_R0, l_scen1_train_G, l_scen1_test_G = fun.split_by_index_R0_G(D['t'+str(t)]['R0']['s1'], D['t'+str(t)]['G']['s1'], index_test=index_test_s1[j])  # scen1
+        l_scen2_train_R0, l_scen2_test_R0, l_scen2_train_G, l_scen2_test_G = fun.split_by_index_R0_G(D['t'+str(t)]['R0']['s2'], D['t'+str(t)]['G']['s2'], index_test=index_test_s2[j])  # scen2
+        m_scen1_train, ld_scen1, il_scen1 = fun.arguments_gp_R0_G(H, phi, l_scen1_train_R0, l_scen1_train_G)
+        m_scen2_train, ld_scen2, il_scen2 = fun.arguments_gp_R0_G(H, phi, l_scen2_train_R0, l_scen2_train_G)
+
+        X_train = pd.concat([fun.ltodf(l_scen1_train_R0 + l_scen2_train_R0), fun.ltodf(l_scen1_train_G + l_scen2_train_G)], axis=1)
+        X_test = pd.concat([fun.ltodf(l_scen1_test_R0 + l_scen2_test_R0), fun.ltodf(l_scen1_test_G + l_scen2_test_G)], axis=1)
+        y_train = np.concatenate((np.repeat(0, len(l_scen1_train_R0)), np.repeat(1, len(l_scen2_train_R0))), axis=None)
+        y_test = np.concatenate((np.repeat(0, len(l_scen1_test_R0)), np.repeat(1, len(l_scen2_test_R0))), axis=None)
+
+        list_probs_gp = []
+        pred_gp = np.zeros(shape=(200,))
+        for i in range(200):
+            list_probs_gp.append(fun.probabilities_gp([fun.LogCP2(X_test.iloc[i].values, m_scen1_train, ld_scen1, il_scen1, 0.5).value(),
+                                                       fun.LogCP2(X_test.iloc[i].values, m_scen2_train, ld_scen2, il_scen2, 0.5).value()]))
+            pred_gp[i] = fun.classif(list_probs_gp[-1])
+
+        proba_gp = np.asarray(list_probs_gp)
+
+        classifier_knn = KNeighborsClassifier(n_neighbors=10)
+        classifier_knn.fit(X_train, y_train)
+        proba_knn = classifier_knn.predict_proba(X_test)
+        pred_knn = classifier_knn.predict(X_test)
+
+        classifier_rf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
+        classifier_rf.fit(X_train, y_train)
+        proba_rf = classifier_rf.predict_proba(X_test)
+        pred_rf = classifier_rf.predict(X_test)
+
+        classifier_mlp = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(20,), random_state=1)
+        classifier_mlp.fit(X_train, y_train)
+        proba_mlp = classifier_mlp.predict_proba(X_test)
+        pred_mlp = classifier_mlp.predict(X_test)
+
+        #CNN: keras
+        X_train_R0 = X_train.values[...,0:324].reshape(800, 18, 18, 1)
+        X_train_G =  X_train.values[...,324:].reshape(800, 18, 18, 1)
+        train_images = np.zeros(shape=(800,18,18,2))
+        train_images[...,0] = X_train_R0.reshape(800,18,18)
+        train_images[...,1] = X_train_R0.reshape(800,18,18)
+
+        X_test_R0 = X_test.values[...,0:324].reshape(200, 18, 18, 1)
+        X_test_G =  X_test.values[...,324:].reshape(200, 18, 18, 1)
+        test_images = np.zeros(shape=(200,18,18,2))
+        test_images[...,0] = X_test_R0.reshape(200,18,18)
+        test_images[...,1] = X_test_R0.reshape(200,18,18)
+
+        train_labels =y_train
+        test_labels = y_test
+
+        model = models.Sequential()
+        model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(18, 18, 2)))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(layers.Flatten())
+        model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dense(1, activation='sigmoid'))
+
+        model.compile(loss='binary_crossentropy',
+                      optimizer=optimizers.RMSprop(lr=1e-4),
+                      metrics=['acc'])
+        model.fit(train_images, train_labels, epochs=60, batch_size=16)
+        proba_cnn = model.predict(test_images)
+        pred_cnn = model.predict_classes(test_images).reshape(200)
+
+        gp_array[t*200:(t+1)*200] = proba_gp[...,1].reshape(200,1)
+        knn_array[t*200:(t+1)*200] = proba_knn[...,1].reshape(200,1)
+        rf_array[t*200:(t+1)*200] = proba_rf[...,1].reshape(200,1)
+        mlp_array[t*200:(t+1)*200] = proba_mlp[...,1].reshape(200,1)
+        cnn_array[t*200:(t+1)*200] = proba_cnn
+
+        values = {'v(x=0,a=0)': -25 + 0.6*times[t], 'v(x=1,a=0)': -27 - 0.6*times[t], 'v(x=0,a=1)': -10, 'v(x=1,a=1)': -42}
+
+        voi_dict['t' + str(t) + '_voi_' + 'GP'] = fun.voi_mc(proba_gp, values)
+        voi_dict['t' + str(t) + '_voi_' + 'knn'] = fun.voi_mc(proba_knn, values)
+        voi_dict['t' + str(t) + '_voi_' + 'rf'] = fun.voi_mc(proba_rf, values)
+        voi_dict['t' + str(t) + '_voi_' + 'mlp'] = fun.voi_mc(proba_mlp, values)
+        voi_dict['t' + str(t) + '_voi_' + 'cnn'] = fun.voi_mc(np.concatenate((np.array(1-proba_cnn), proba_cnn), axis=1), values)
+
+        accuracy_dict['t' + str(t) + '_accuracy_' + 'GP'] = accuracy_score(y_test, pred_gp)
+        accuracy_dict['t' + str(t) + '_accuracy_' + 'knn'] = accuracy_score(y_test, pred_knn)
+        accuracy_dict['t' + str(t) + '_accuracy_' + 'rf'] = accuracy_score(y_test, pred_rf)
+        accuracy_dict['t' + str(t) + '_accuracy_' + 'mlp'] = accuracy_score(y_test, pred_mlp)
+        accuracy_dict['t' + str(t) + '_accuracy_' + 'cnn'] = accuracy_score(y_test, pred_cnn)
+
+    probas_gp_array[..., j] = gp_array.reshape(1600)
+    probas_knn_array[..., j] = knn_array.reshape(1600)
+    probas_rf_array[..., j] = rf_array.reshape(1600)
+    probas_mlp_array[..., j] = mlp_array.reshape(1600)
+    probas_cnn_array[..., j] = cnn_array.reshape(1600)
+
+    voi_array[5:, j]=np.asarray([x for x in voi_dict.values()]).reshape(40,)
+    accuracy_array[5:, j]=np.asarray([x for x in accuracy_dict.values()]).reshape(40,)
